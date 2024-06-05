abc2aa460d749c0d3cc86a3058d5a27d55401476
Francisco <franciscommvieira@hotmail.com>
2024-06-05 19:26:06 +0100

    ex01 done ex02 started

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -50,7 +50,7 @@ void	RPN::trim(char *str)
 		else
 		{
 			if (token[0] >= '0' && token[0] <= '9')
-    			input.push_back(atoi(token[0]));
+    			input.push_back(token[0] - '0');
 			else if ((token[0] == '-' || token[0] == '+' || token[0] == '/'
 				|| token[0] == '*') && input.size() >= 2)
 				operations(token[0]);
@@ -60,17 +60,23 @@ void	RPN::trim(char *str)
 				return ;
 			}
 
-			//std::cout << token[0] << std::endl;
 		}
     	s.erase(0, pos + delimiter.length());
 	}
+	if (input.size() > 1)
+	{
+		std::cout << "Error\n";
+		return ;
+	}
+	else
+		std::cout << input.front() << std::endl;
 }
 
 void	RPN::operations(char op)
 {
-	int	v1 = input.back();
-	input.pop_back();
 	int	v2 = input.back();
+	input.pop_back();
+	int	v1 = input.back();
 	input.pop_back();
 	switch (op)
 	{
@@ -78,7 +84,13 @@ void	RPN::operations(char op)
 			input.push_back(v1 * v2);
 			break;
 		case '/':
-			input.push_back(v1 / v2);
+			if (v2 == 0)
+			{
+				std::cout << "Error\n";
+				exit(1);
+			}
+			else
+				input.push_back(v1 / v2);
 			break;
 		case '+':
 			input.push_back(v1 + v2);
